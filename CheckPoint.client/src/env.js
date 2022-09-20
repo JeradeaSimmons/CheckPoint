404dbb8691f0326006895f890204f7395566724b
JD Simmons <hippeejd@gmail.com>
2022-09-20 15:14:27 -0600

    some

--- CheckPoint.client/src/env.js
+++ CheckPoint.client/src/env.js
@@ -1,8 +1,7 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:5001' : ''
+export const baseURL = dev ? 'https://localhost:5001' : ''
 export const useSockets = false
 
-// GRAB AUTH FROM OLD PROJECT
 export const domain = 'hippeejd.us.auth0.com'
 export const clientId = 'sC7bCqbQi9X5OKrtZJ5k9v8NWL6WpLHB'
 export const audience = 'https://mycodeworks.com'
